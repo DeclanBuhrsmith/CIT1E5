a832ba5873f5db433cf63f18ff53381a9a442cca
Declan Buhrsmith <DBuhrsmith@zebra.com>
2023-12-18 16:43:48 -0600

    Added google api to landing page.

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 
 
@@ -9,7 +10,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   declarations: [LandingPageComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    GoogleMapsModule
   ], exports: [LandingPageComponent]
 })
 export class LandingPageModule { }
