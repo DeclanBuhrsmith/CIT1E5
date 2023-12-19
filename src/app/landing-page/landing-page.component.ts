04b6d02ec5bd323a6eec910505166c20b2758deb
Declan Buhrsmith <DBuhrsmith@zebra.com>
2023-12-19 17:34:41 -0600

    theming

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { GoogleMap } from '@angular/google-maps';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'landing-page',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  @HostBinding('class') className = '';
+  @ViewChild('map') map: GoogleMap | undefined;
+  toggleControl = new FormControl(false);
 
+  constructor(private overlay: OverlayContainer) { }
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? darkClassName : '';
+      if (darkMode) {
+        this.overlay.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlay.getContainerElement().classList.remove(darkClassName);
+      }
+    });
+  }
 }
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { GoogleMapsModule } from '@angular/google-maps';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
@@ -11,7 +12,10 @@ import { GoogleMapsModule } from '@angular/google-maps';
   imports: [
     CommonModule,
     MatToolbarModule,
-    GoogleMapsModule
-  ], exports: [LandingPageComponent]
+    GoogleMapsModule,
+    MatIconModule,
+    MatSlideToggleModule
+  ], exports: [LandingPageComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LandingPageModule { }
